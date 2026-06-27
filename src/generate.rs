a3d4ea612a239ad915f35285a659f0bf0f05d417
slbsh <crafter.antek@gmail.com>
2026-06-27 22:54:19 +0200

    feat: footnotes + codebase rework
    Co-authored-by: Nick Bors nick@nickbors.cc

--- src/generate.rs
+++ src/generate.rs
@@ -1,6 +1,6 @@
-use comrak::{parse_document, Arena, Options};
+use comrak::{Arena, Options};
 use comrak::adapters::SyntaxHighlighterAdapter;
-use comrak::nodes::{AstNode, NodeCode, NodeCodeBlock, NodeHeading, NodeHtmlBlock, NodeLink, NodeList, NodeMath, NodeValue};
+use comrak::nodes::{AstNode, NodeCode, NodeCodeBlock, NodeFootnoteDefinition, NodeFootnoteReference, NodeHeading, NodeHtmlBlock, NodeLink, NodeList, NodeMath, NodeValue};
 use comrak::plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder};
 
 use syntect::highlighting::{ThemeSet, Theme, ThemeSettings, ThemeItem, ScopeSelector, ScopeSelectors, StyleModifier, Color, FontStyle};
@@ -19,54 +19,14 @@ use std::path::{Path, PathBuf};
 use std::io::Write as _;
 use std::fmt::Write as _;
 
-// workaround since syntax is loaded in a static and I cant be bothered to move it
-static SYNTAX_DIR: OnceLock<PathBuf> = OnceLock::new();
+static INCLUDE_RE:    LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"<include "(.*)">"#).unwrap());
+static BLOG_ENTRY_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"<blog-header "(.*)", "(.*)", "(.*)">"#).unwrap());
 
 pub fn generate(src: &Path, dst: &Path, syntax: PathBuf) {
-	static BLOG_ENTRY_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"<blog-header "(.*)", "(.*)", "(.*)">"#).unwrap());
-	static INCLUDE_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r#"<include "(.*)">"#).unwrap());
+	const BLOG_DIR:   &str = "blog";
+	const ASSETS_DIR: &str = "assets";
 
 	SYNTAX_DIR.get_or_init(|| syntax);
-
-	let resolve_macros = |mut input: String| {
-		let f  = |c: &Captures| fs::read_to_string(src.join(&c[1])).unwrap();
-		if let Cow::Owned(s) = INCLUDE_RE.replace_all(&input, f) { input = s; }
-
-		let f  = |c: &Captures| format!(r#"<div class="blog-header"><h2>{}</h2><p>by {}</b><p class="blog-date">{}</p></div>"#, &c[1], &c[2], &c[3]);
-		if let Cow::Owned(s) = BLOG_ENTRY_RE.replace_all(&input, f) { input = s; }
-
-		input
-	};
-
-	let options = Options {
-		extension: comrak::ExtensionOptions {
-			strikethrough: true,
-			table: true,
-			superscript: true,
-			footnotes: true,
-			multiline_block_quotes: true,
-			math_dollars: true,
-			underline: true,
-			spoiler: true,
-			..Default::default()
-		},
-		..Default::default()
-	};
-
-	let into_html = |path: &Path| {
-		let arena = Arena::new();
-		let root = parse_document(&arena,
-			&std::fs::read_to_string(path).unwrap(),
-			&options
-		);
-
-		format!("<!DOCTYPE html>\n<html lang=en>\n<meta charset=\"UTF-8\">\n<link href=\"/theme.css\" rel=\"stylesheet\"/>\n{}\n</html>", parse_block(root))
-	};
-
-	let mkfile = |path: &Path| {
-		fs::create_dir_all(path.parent().unwrap()).unwrap();
-		fs::File::create(path).unwrap()
-	};
 
 	if dst.exists() { 
 		fs::remove_dir_all(dst).unwrap(); 
@@ -74,22 +34,36 @@ pub fn generate(src: &Path, dst: &Path, syntax: PathBuf) {
 
 	fs::create_dir(dst).unwrap();
 
-	let blog_dir = src.join("blog");
+	let blog_dir = src.join(BLOG_DIR);
 
+	let resolve_macros = |mut i: String| {
+		let f  = |c: &Captures| fs::read_to_string(src.join(&c[1])).unwrap();
+		if let Cow::Owned(s) = INCLUDE_RE.replace_all(&i, f) { i = s; }
+
+		let f  = |c: &Captures| format!(r#"<div class="blog-header"><h2>{}</h2><p>by {}</b><p class="blog-date">{}</p></div>"#, &c[1], &c[2], &c[3]);
+		if let Cow::Owned(s) = BLOG_ENTRY_RE.replace_all(&i, f) { i = s; }
+
+		i
+	};
+
+	//
+	// process all files
 	walkdir::WalkDir::new(src).into_iter()
 		.filter_map(Result::ok).filter(|e| !e.path().is_dir()).for_each(|e| {
 			let path = e.path();
 			let dest = dst.join(path.iter().skip(1).collect::<PathBuf>());
 
-			if path.starts_with(&blog_dir) && e.file_name() == "assets" { return; }
+			if path.starts_with(&blog_dir) && e.file_name() == ASSETS_DIR { return; }
 
 			match path.extension().and_then(|s| s.to_str()) {
-				Some("md")   => mkfile(&dest.with_extension("html")).write_all(resolve_macros(into_html(path)).as_bytes()),
+				Some("md")   => mkfile(&dest.with_extension("html")).write_all(resolve_macros(Parser::parse(path)).as_bytes()),
 				Some("html") => mkfile(&dest).write_all(resolve_macros(fs::read_to_string(path).unwrap()).as_bytes()),
 				_            => mkfile(&dest).write_all(&fs::read(path).unwrap()),
 			}.unwrap();
 		});
 
+	//
+	// blogs
 	let mut posts = std::fs::read_dir(&blog_dir).unwrap().filter_map(|e| {
 			let path = e.ok()?.path();
 
@@ -99,7 +73,7 @@ pub fn generate(src: &Path, dst: &Path, syntax: PathBuf) {
 			let c = BLOG_ENTRY_RE.captures(&content)?;
 			let (name, auth, date) = (c[1].to_string(), c[2].to_string(), c[3].to_string());
 
-			let content = into_html(&path);
+			let content = Parser::parse(&path);
 			let newpath = dst.join(path.iter().skip(1).collect::<PathBuf>()).with_extension("html");
 			mkfile(&newpath).write_all(resolve_macros(content.clone()).as_bytes());
 
@@ -126,133 +100,199 @@ pub fn generate(src: &Path, dst: &Path, syntax: PathBuf) {
 		},
 	};
 
-	let blog_idx = into_html(&blog_dir.join("index.md"))
+	let blog_idx = Parser::parse(&blog_dir.join("index.md"))
 		.replacen("<latest>", &latest, 1)
 		.replacen("<allposts>", &blog_list, 1);
 
-	mkfile(&dst.join("blog/index.html")).write_all(resolve_macros(blog_idx).as_bytes());
+	mkfile(&dst.join(BLOG_DIR).join("index.html")).write_all(resolve_macros(blog_idx).as_bytes());
 }
 
-fn parse_block<'a>(node: &'a AstNode<'a>) -> String {
-	let to_string = |node: &'a AstNode<'a>|
-		node.children().map(parse_block).collect();
 
-	match node.data.borrow().value {
-		NodeValue::Heading(NodeHeading { level, ..}) => {
-			let str: String = to_string(node);
-			format!("\n<h{level} id=\"{}\">{}</h{level}>\n", 
-				str.to_lowercase().split_whitespace().collect::<String>(), str)
-		},
-		NodeValue::Table(..) => {
-			let mut out = String::from("<table>\n");
+fn mkfile(path: &Path) -> fs::File {
+	fs::create_dir_all(path.parent().unwrap()).unwrap();
+	fs::File::create(path).unwrap()
+}
 
-			// Required as per GFM spec to not generate bodies when a table row hasn't been seen.
-			let mut seen_header = false;
-			let mut seen_body   = false;
+struct Parser<'p> {
+	footnotes: Vec<(String, Option<String>)>,
+	path: &'p Path
+}
 
-			for row in node.children() {
-				let NodeValue::TableRow(header) = row.data.borrow().value else {
-					eprintln!("WARNING: expected only TableRows as direct children of Table!");
-					continue;
+impl<'a, 'p> Parser<'p> {
+	fn parse(path: &'p Path) -> String {
+		static OPTIONS: LazyLock<Options> = LazyLock::new(|| Options {
+			extension: comrak::ExtensionOptions {
+				strikethrough: true,
+				table: true,
+				superscript: true,
+				footnotes: true,
+				multiline_block_quotes: true,
+				math_dollars: true,
+				underline: true,
+				spoiler: true,
+				..Default::default()
+			},
+			..Default::default()
+		});
+
+		let arena = Arena::new();
+		let root = comrak::parse_document(&arena, &fs::read_to_string(path).unwrap(), &OPTIONS);
+
+		let mut this = Self { footnotes: Vec::new(), path };
+		let mut out = this.parse_node(root);
+
+		if let Some(marker) = out.find("<footnotes>") && !this.footnotes.is_empty() {
+			let footnotes = this.footnotes.iter().fold(String::new(), |s, (name, desc)| {
+				let Some(desc) = desc else {
+					println!("WARN: [{}] missing footnote description for ref `{name}`", this.path.display());
+					return s;
 				};
 
-				if header && !seen_header {
-					writeln!(out, "<thead>");
-					seen_header = true;
-				}
-
-				if !seen_body && seen_header && !header {
-					writeln!(out, "</thead>\n<tbody>");
-					seen_body = true;
-				}
-
-				writeln!(out, "<tr>");
-
-				row.children().for_each(|cell| {
-					if !matches!(cell.data.borrow().value, NodeValue::TableCell) {
-						eprintln!("WARNING: expected only TableCells as direct children of TableRow!");
-						return;
-					}
-
-					match header {
-						true => writeln!(out, "<th>{}</th>", to_string(cell)),
-						_    => writeln!(out, "<td>{}</td>", to_string(cell)),
-					};
-				});
-
-				writeln!(out, "</tr>");
-			}
-
-			if !seen_body && seen_header {
-				writeln!(out, "</thead>");
-			}
-
-			writeln!(out, "</table>\n");
-			out
-		},
-		// we are fancy and use em dashes, not just hyphens
-		NodeValue::Text(ref text) => String::from(text).replace("--", "—"), 
-		NodeValue::LineBreak      => String::from("<br>\n"),
-		NodeValue::SoftBreak      => String::from(" \n"),
-		NodeValue::Paragraph      => format!("<p>{}</p>\n", to_string(node)),
-		NodeValue::BlockQuote     => format!("\n<div class=quote>{}</div>\n", to_string(node)),
-		NodeValue::Strong         => format!("<b>{}</b>", to_string(node)),
-		NodeValue::Emph           => format!("<i>{}</i>", to_string(node)),
-		NodeValue::Strikethrough  => format!("<s>{}</s>", to_string(node)),
-		NodeValue::HtmlInline(ref html) => String::from(html),
-		NodeValue::HtmlBlock(NodeHtmlBlock { ref literal, .. }) => String::from(literal),
-		NodeValue::Code(NodeCode { ref literal, .. }) => format!("<c>{literal}</c>"),
-		NodeValue::Link(NodeLink { ref url, .. }) => format!("<a href=\"{url}\">{}</a>", to_string(node)), 
-		NodeValue::Image(NodeLink { ref url, .. }) => format!("<img src=\"{url}\" alt=\"{}\">", to_string(node)),
-		NodeValue::List(NodeList { ref bullet_char, .. }) => format!("<div class=block>\n{}</div>\n", 
-			node.children()
-				.map(|n| format!("<p>{}{}</p>\n", if *bullet_char as char == '-' { "- " } else { "" },
-					if let Some(n) = n.children().next()
-						.filter(|n| n.data.borrow().value == NodeValue::Paragraph)
-					{ to_string(n) } else { parse_block(n) }))
-				.collect::<String>()),
-		NodeValue::CodeBlock(NodeCodeBlock { ref literal, .. }) => {
-			let mut out = Vec::new();
-
-			ADAPTER
-				.write_highlighted(&mut out, Some("shard"), literal)
-				.unwrap();
-
-			let str = String::from_utf8(out).unwrap();
-
-			static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new("color:#([0-9a-f]{2})0000;").unwrap());
-			let str = RE.replace_all(&str, |c: &Captures|
-				format!("color:var(--colour{});", usize::from_str_radix(&c[1], 16).unwrap())
-			);
-
-			format!("<code><pre>\n{str}</pre></code>\n")
-		},
-		NodeValue::Math(NodeMath { ref literal, .. }) => {
-			static KATEX_CTX: LazyLock<KatexContext> = LazyLock::new(KatexContext::default);
-
-			let settings = Settings::builder()
-				.output(katex::OutputFormat::Mathml)
-				.display_mode(false)
-				.build();
-
-			let out = katex::render_to_string(&*KATEX_CTX, literal, &settings).unwrap_or_else(|e| {
-				eprintln!("Failed to render math: {e}");
-				format!("${literal}$")
+				format!("{s}\n<p class=\"footnote-def\" id=\"footnote-{name}\">{name}: {desc}</p>")
 			});
 
-			format!("{out}\n")
-		},
-		_ => to_string(node),
+			out.replace_range(marker..=marker+"<footnotes>".len(), &footnotes);
+		}
+
+		format!("<!DOCTYPE html>\n<html lang=en>\n<meta charset=\"UTF-8\">\n<link href=\"/theme.css\" rel=\"stylesheet\"/>\n{out}\n</html>")
+	}
+
+	fn to_string(&mut self, node: &'a AstNode<'a>) -> String {
+		node.children().map(|n| self.parse_node(n)).collect()
+	}
+
+	fn parse_node(&mut self, node: &'a AstNode<'a>) -> String {
+		match node.data.borrow().value {
+			NodeValue::Heading(NodeHeading { level, ..}) => {
+				let str: String = self.to_string(node);
+				format!("\n<h{level} id=\"{}\">{}</h{level}>\n", 
+					str.to_lowercase().split_whitespace().collect::<String>(), str)
+			},
+			NodeValue::Table(..) => {
+				let mut out = String::from("<table>\n");
+
+				// Required as per GFM spec to not generate bodies when a table row hasn't been seen.
+				let mut seen_header = false;
+				let mut seen_body   = false;
+
+				for row in node.children() {
+					let NodeValue::TableRow(header) = row.data.borrow().value else {
+						eprintln!("WARNING: expected only TableRows as direct children of Table!");
+						continue;
+					};
+
+					if header && !seen_header {
+						writeln!(out, "<thead>");
+						seen_header = true;
+					}
+
+					if !seen_body && seen_header && !header {
+						writeln!(out, "</thead>\n<tbody>");
+						seen_body = true;
+					}
+
+					writeln!(out, "<tr>");
+
+					row.children().for_each(|cell| {
+						if !matches!(cell.data.borrow().value, NodeValue::TableCell) {
+							eprintln!("WARNING: expected only TableCells as direct children of TableRow!");
+							return;
+						}
+
+						match header {
+							true => writeln!(out, "<th>{}</th>", self.to_string(cell)),
+							_    => writeln!(out, "<td>{}</td>", self.to_string(cell)),
+						};
+					});
+
+					writeln!(out, "</tr>");
+				}
+
+				if !seen_body && seen_header {
+					writeln!(out, "</thead>");
+				}
+
+				writeln!(out, "</table>\n");
+				out
+			},
+			// we are fancy and use em dashes, not just hyphens
+			NodeValue::Text(ref text) => String::from(text).replace("--", "—"), 
+			NodeValue::LineBreak      => String::from("<br>\n"),
+			NodeValue::SoftBreak      => String::from(" \n"),
+			NodeValue::Paragraph      => format!("<p>{}</p>\n", self.to_string(node)),
+			NodeValue::BlockQuote     => format!("\n<div class=quote>{}</div>\n", self.to_string(node)),
+			NodeValue::Strong         => format!("<b>{}</b>", self.to_string(node)),
+			NodeValue::Emph           => format!("<i>{}</i>", self.to_string(node)),
+			NodeValue::Strikethrough  => format!("<s>{}</s>", self.to_string(node)),
+			NodeValue::HtmlInline(ref html) => String::from(html),
+			NodeValue::HtmlBlock(NodeHtmlBlock { ref literal, .. }) => String::from(literal),
+			NodeValue::Code(NodeCode { ref literal, .. }) => format!("<c>{literal}</c>"),
+			NodeValue::Link(NodeLink { ref url, .. }) => format!("<a href=\"{url}\">{}</a>", self.to_string(node)), 
+			NodeValue::Image(NodeLink { ref url, .. }) => format!("<img src=\"{url}\" alt=\"{}\">", self.to_string(node)),
+			NodeValue::List(NodeList { ref bullet_char, .. }) => format!("<div class=block>\n{}</div>\n", 
+				node.children()
+					.map(|n| format!("<p>{}{}</p>\n", if *bullet_char as char == '-' { "- " } else { "" },
+						if let Some(n) = n.children().next()
+							.filter(|n| n.data.borrow().value == NodeValue::Paragraph)
+						{ self.to_string(n) } else { self.parse_node(n) }))
+					.collect::<String>()),
+			NodeValue::CodeBlock(NodeCodeBlock { ref literal, .. }) => {
+				let mut out = Vec::new();
+
+				ADAPTER
+					.write_highlighted(&mut out, Some("shard"), literal)
+					.unwrap();
+
+				let str = String::from_utf8(out).unwrap();
+
+				static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new("color:#([0-9a-f]{2})0000;").unwrap());
+				let str = RE.replace_all(&str, |c: &Captures|
+					format!("color:var(--colour{});", usize::from_str_radix(&c[1], 16).unwrap())
+				);
+
+				format!("<code><pre>\n{str}</pre></code>\n")
+			},
+			NodeValue::Math(NodeMath { ref literal, .. }) => {
+				static KATEX_CTX: LazyLock<KatexContext> = LazyLock::new(KatexContext::default);
+
+				let settings = Settings::builder()
+					.output(katex::OutputFormat::Mathml)
+					.display_mode(false)
+					.build();
+
+				let out = katex::render_to_string(&KATEX_CTX, literal, &settings).unwrap_or_else(|e| {
+					eprintln!("Failed to render math: {e}");
+					format!("${literal}$")
+				});
+
+				format!("{out}\n")
+			},
+			NodeValue::FootnoteReference(NodeFootnoteReference { ref name, .. }) => {
+				if !self.footnotes.iter().any(|(n, _)| n == name) {
+					self.footnotes.push((name.clone(), None));
+				}
+
+				format!("<a class=\"footnote-ref\" href=\"#footnote-{name}\"><sup>[{name}]</sup></a>")
+			},
+			NodeValue::FootnoteDefinition(NodeFootnoteDefinition { ref name, .. }) => {
+				let desc = node.children().map(|x| self.to_string(x)).collect::<String>();
+
+				self.footnotes.iter_mut()
+					.find(|(s, _)| s == name)
+					.map_or_else(
+						|| println!("WARN: [{}] missing ref for footnote definition `{name}`", self.path.display()),
+						|(_, d)| *d = Some(desc));
+
+				String::new()
+			},
+			_ => self.to_string(node)
+		}
 	}
 }
 
+static SYNTAX_DIR: OnceLock<PathBuf> = OnceLock::new();
 static ADAPTER: LazyLock<SyntectAdapter> = LazyLock::new(|| {
-	let color = |val: u8| Color {
-		r: val,
-		g: 0,
-		b: 0,
-		a: 0xff,
-	};
+	let color = |r: u8| Color { r, g: 0, b: 0, a: 0xff };
 
 	let item = |path: &str, hex: u8, style: Option<FontStyle>| ThemeItem {
 		scope: ScopeSelectors { selectors: vec![ScopeSelector { path: ScopeStack::from_vec(vec![Scope::new(path).unwrap()]), excludes: Vec::new()}]},
@@ -284,7 +324,7 @@ static ADAPTER: LazyLock<SyntectAdapter> = LazyLock::new(|| {
 			item("keyword.move",       7,  Some(FontStyle::ITALIC)),
 
 			item("entity.type",        5,  None),
-         item("entity.type.named",  5,  None),
+			item("entity.type.named",  5,  None),
 			item("entity.type.generic",5,  Some(FontStyle::BOLD)),
 			item("entity.type.lesser", 5,  Some(FontStyle::ITALIC)),
 			item("entity.function",    6,  None),
